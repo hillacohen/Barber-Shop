485efc71da2fc71fa635ac6bacf3db959beaf867
Hilla Cohen <hillac@consist-sys.consist.co.il>
2019-05-27 19:28:26 +0300

    Add project files.

---
+++ BarberShop/Models/DBConnector.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DBTest
+{
+    public  class DBConnector
+    {
+        private string connectionString= "Data Source=LT10-HILLAC\\PRINTBOS;Initial Catalog=newdb;Integrated Security=True";
+
+        public DBConnector()
+        {
+            try
+            {
+                //String connectionString = "Data Source=LT10-HILLAC\\PRINTBOS;Initial Catalog=newdb;Integrated Security=True";
+                // string connetionString;
+                SqlConnection cnn;
+                cnn = new SqlConnection(connectionString);
+                cnn.Open();
+               
+                cnn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+
+        public DataTable SelectAll(string table)
+        {
+            DataTable dt = new DataTable();
+            string query = $"SELECT * FROM {table}";
+            using (SqlConnection conn=new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query,conn);
+                command.Connection.Open();
+               // command.ExecuteNonQuery();
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                da.Fill(dt);
+            }
+            return dt;
+        }
+
+        public void AddUser(string Username, string name,string password)
+        {
+            
+            DataTable dt = new DataTable();
+            string query = $"INSERT INTO [BarberShopDB].[dbo].[Users] (UserName, Name, Password) VALUES('{Username}', '{name}', '{password}'); ";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Connection.Open();
+                 command.ExecuteNonQuery();
+             
+               
+            }
+           
+        }
+
+        public void RemoveUser(string Username)
+        {
+
+            DataTable dt = new DataTable();
+            string query = $" DELETE FROM [BarberShopDB].[dbo].[Users] WHERE Name={Username}";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+        }
+      
+        public void IfUserNameExist(string Username)
+        {
+
+            DataTable dt = new DataTable();
+            string query = $"SELECT count (*) FROM[BarberShopDB].[dbo].[Users] where UserName = '{Username}'";
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
+
+        }
+
+
+    }
+}
---
+++ BarberShop/Models/Validation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DBTest
+{
+    public static class Validation
+    {
+        //חייבת להכיל לפחות 8 תווים, לא יותר יותר מ15 תווים, לפחות אות קטנה אחת, לפחות ספרה אחת, לפחות גדולה אחת
+        public static bool IsPasswordValid(string password)
+        {
+            string passwordStr = new System.Net.NetworkCredential(string.Empty, password).Password;
+            string pattern = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[^\\da-zA-Z]).{8,15}$";
+            Regex regex = new Regex(pattern);
+            return regex.IsMatch(passwordStr);
+        }
+
+
+    }
+}
